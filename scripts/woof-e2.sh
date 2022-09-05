376a0720f235c73f9724b8ace4f34c83ebb48d97
Erin Rivas <erin@atomicbear.com>
2022-09-04 21:04:20 -0500

    small tweaks

--- scripts/woof-e2.sh
+++ scripts/woof-e2.sh
@@ -18,7 +18,7 @@ canined init $MONIKER --chain-id $CHAIN
 canined keys add $ALIAS --keyring-backend="test"
 canined config keyring-backend test
 
-canined add-genesis-account $(canined keys show $ALIAS -a) 250000000ujkl
+canined add-genesis-account $(canined keys show $ALIAS -a) 250000000ujkl --chain-id $CHAIN
 canined gentx $ALIAS 200000000ujkl --chain-id $CHAIN
 
 sed -i 's/mode = "full"/mode = "validator"/g' $HOME/.canine/config/config.toml
@@ -26,3 +26,5 @@ sed -i 's/indexer = \["null"\]/indexer = \["kv"\]/g' $HOME/.canine/config/config
 
 KEY=$(jq '.pub_key' ~/.canine/config/priv_validator_key.json -c)
 echo $KEY > ~/key.json
+
+echo "done running"
